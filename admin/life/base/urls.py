449e243523b8ab0d90c72db7041f5b9163d5a274
Sdu <sdu@shendu.info>
2018-12-30 14:26:18 +0800

    bug fix

--- admin/life/base/urls.py
+++ admin/life/base/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, re_path
 
-from life.base.views import (index, data, chart, analytics, )
+from life.base.views import (index, data, analytics, )
 from life.base.api_views import(DataView, )
 
 urlpatterns = [
@@ -10,6 +10,5 @@ urlpatterns = [
 
     path('api/index', DataView.as_view()),
     path('api/data', DataView.as_view()),
-    path('api/chart', DataView.as_view()),
     path('api/analytics', DataView.as_view()),
 ]
